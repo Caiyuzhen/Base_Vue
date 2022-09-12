520a3584e14bef959bb3986c35b962e130358e29
yuzhen <caiyuzhen00@gmail.com>
2022-09-12 17:12:30 +0800

    update

--- components/AppSection.js
+++ components/AppSection.js
@@ -1,61 +1,87 @@
 import AppSectionList from "/components/AppSectionList.js"
 
 export default {
+
+	// 👇注册组件
 	components: { AppSectionList },
-	// 🔥ES6-html 插件, 可以更好的显示 html 代码
-	template: /*html*/
-	`
-		<!-- 👛 未购零食
-				在子组件内定义  headline 的 prop 属性, 然后就可以在这个【父组件内】传值（使用父组件的数据）了
-					⚡️分别访问两组数据:	filtersFn -> afterBuy
-		-->
-		<app-section-list
-			headline="👛 Did not purchase"
-			v-bind:BuyChild="filtersFn.beforeBuy"
-			>
-		</app-section-list>
+
+
+
+	// 🔥组件渲染模板, 使用 ES6-html 插件, 可以更好的显示 html 代码
+	template: /*html*/`
+		<!-- 父组件传递数据给子组件步骤：
+				1.在【引用了子组件】的【父组件内】定义 byChild 这个 prop 属性，并且传入 beforeBuy 、 afterBuy 两个表达式
+				2.在【子组件】内定义 byChild 这个 prop 属性，并且在设置 byChild 属性的数据类型 -->
+
+
+		<!-- 👛 未购零食: 🔥 在子组件内定义  headline 的 prop 属性, 然后就可以在这个【父组件内】传值（使用父组件的数据）了, 然后⚡️分别访问两组数据:	filtersFn -> afterBuy-->
+
+		<!--👇用 v-on 给输入框绑定事件, 用 :class="{buttonColor: true} 给元素绑定 css 类 用 v-if, v-else-if, v-else 来判断不同的状态, v-else 不需要接属性值-->
+		<form v-on:submit.prevent="add">
+			<input v-bind:class="{inputBar: true}" type="text" placeholder="input something..." v-model="newFood"/>
+			
+			<button :class="{buttonColor_gray: true}" type="submit" v-if="foods.length <= 3">👉 Add foods</button>
+			<button :class="{buttonColor_black: true}"  type="submit" v-else-if="foods.length > 3   &&   foods.length < 5">🤙 Add more foods</button>
+			<button :class="{buttonColor_red: true}" type="submit" v-else>🔥 Keep add foods</button>
+		</form>
 
 		
-		<!--  💵 已购零食
-					🔥 v-show 用数组返回的 length 长度来判断是否该显示
-						⚡️分别访问两组数据:	filtersFn -> afterBuy
-		 -->
-		<app-section-list
-			headline="💵 Purchased Snack List"
-			v-bind:BuyChild="filtersFn.afterBuy" 
-			>
-		</app-section-list>
-	
+		<app-section-list headline="👛 Did not purchased" v-bind:buyChild="filtersFn.beforeBuy"> </app-section-list>
+		
+		<!--  💵 已购零食: 🔥 用 v-show 判断数组返回的 length 长度来判断是否该显示, 然后⚡️分别访问两组数据:	filtersFn -> afterBuy-->
+		<app-section-list headline="💵 Purchased Snack List" v-bind:buyChild="filtersFn.afterBuy" > </app-section-list>
+
 	`,
+	
 
-		data(){//在 data 函数内返回一个对象, 可以存放数据
+
+
+	// 🔥组件身上的数据
+	data(){//在 data 函数内返回一个对象, 可以存放数据
+		return {
+			foods: [
+				{id:1, name: 'bread', image:'./img/bread.png', purchased: false},
+				{id:2, name: 'cake', image:'./img/cake.png', purchased: false},
+				{id:3, name: 'salad', image:'./img/salad.png', purchased: false},
+			],
+			newFood: ''  //🔥用来存放输入框的临时数据
+		}
+	},
+
+
+
+
+	// 🔥抽离出两个表达式
+	computed: {
+		// 👇因为子组件的【一个模板】不能【传两个参数】, 所以把两个方法【整合成一个对象】
+		filtersFn(){
 			return {
-				title: '👛 Did not purchase',
-				title02: '💵 Purchased Snack List',
-				foods: [
-					{id:1, name: 'bread', image:'./img/bread.png', purchased: false},
-					{id:2, name: 'cake', image:'./img/cake.png', purchased: false},
-					{id:3, name: 'salad', image:'./img/salad.png', purchased: false},
-				]
-			}
-		},
-
-		// 抽离出两个表达式
-		computed: {
-			// 👇因为子组件的【一个模板】不能【传两个参数】, 所以把两个方法【整合成一个对象】
-			// beforeBuy() { //未购买的表达式
-			// 	return this.foods.filter(item => !item.purchased)
-			// },
-			// afterBuy() { //未购买的表达式
-			// 	return this.foods.filter(item => item.purchased)
-			// }
-			filtersFn(){
-				return{
-					//未购买的表达式
-					beforeBuy: this.foods.filter(item => !item.purchased),
-					//已购买的表达式
-					afterBuy: this.foods.filter(item => item.purchased)
-				}
+				//未购买的表达式
+				beforeBuy: this.foods.filter(item => !item.purchased),
+				//已购买的表达式
+				afterBuy: this.foods.filter(item => item.purchased)
 			}
 		}
+		/*  beforeBuy() { //未购买的表达式
+		 		return this.foods.filter(item => !item.purchased)
+		 	},
+			afterBuy() { //未购买的表达式
+		 		return this.foods.filter(item => item.purchased)
+			}*/
+	},
+
+
+	// 🔥组件身上的方法
+	methods: {
+		add() {
+			this.foods.push({
+				id: this.foods.length + 1,
+				name: this.newFood,
+				image: '/img/bread.png',
+				purchased: false,
+			})
+			this.newFood = ''// ⚡️ push 完数据后, 清空输入框
+		}
+	}
+
 }
--- components/AppSectionList.js
+++ components/AppSectionList.js
@@ -1,15 +1,16 @@
 export default {
+
 	template: /*html*/
 	`
 		<!-- <section v-show="foods.filter(item => !item.purchased).length"> -->
 		<!-- 简化的写法, 抽离出 beforeBuy 表达式 -->
-		<section v-show="BuyChild">
-			<h2> {{title}} </h2>
+		<!-- 连接到父组件的数据后, 🔥用 {{}} 差值表达式来引用父组件传入的数据！！-->
+		<section v-show="buyChild.length">
+			<h2> {{headline}} </h2>
 			<ul>
 				<!-- 🔥需要 v-bind:key="food.id" 才不会同时勾选多个-->
 				<!-- v-for="food in foods.filter(item => !item.purchased)"  -->
-				<li 
-					v-for="food in BuyChild" 
+				<li v-for="food in buyChild" 
 					v-bind:key="food.id">
 						<img v-bind:src="food.image">
 						<input v-model="food.purchased" type="checkbox">
@@ -21,9 +22,12 @@ export default {
 		</section>
 	`,
 
+	
+	//props 是用于在【子组件】内关联【父组件】数据的方法
+	//在【子组件内】接收一个字符类型的 headline 属性, 然后就可以在【父组件】内设置 【headline 的属性】了, 用来定义标题文字！！
+	//🔥相当于在子组件的 data 内声明了一个 headline、buyChild 数据！！
 	props: {
-		//在【子组件内】接收一个字符类型的 headline 属性, 然后就可以在【父组件】内设置 【headline 的属性】了, 用来定义标题文字！！
-		headline: String, //相当于声明了一个类型
-		BuyChild: Object
+		headline: String, 
+		buyChild: Object
 	}
 }
