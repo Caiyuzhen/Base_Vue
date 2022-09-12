114d2842e6a6d63ec5c39475606cc93dae6fa73f
yuzhen <caiyuzhen00@gmail.com>
2022-09-12 23:10:46 +0800

    update

--- components/AppSection.js
+++ components/AppSection.js
@@ -1,6 +1,8 @@
 import AppSectionList from "/components/AppSectionList.js"
 import AppForm from "/components/AppForm.js"
 
+
+
 export default {
 
 	// 👇注册组件
@@ -9,17 +11,19 @@ export default {
 
 
 	// 🔥组件渲染模板, 使用 ES6-html 插件, 可以更好的显示 html 代码
-	template: /*html*/`
+	template: /*html*/
+	`
 		<!-- 👇父组件传递数据给子组件步骤：
 				1. 在父组件中定义数据(或者是父组件上用【表达式】去获取 data() 中的数据)
 				2.在【子组件】内定义 【buyChild】 这个 【prop】 属性，并且设置 byChild 属性的【数据类型】
 				3.在【父组件内】绑定 【buyChild】 这个 【prop 属性】，并且传入 【beforeBuy 、 afterBuy】两个【表达式】
-				4.在子组件内使用【buyChild】 这个 【prop 属性】
-		-->
+				4.在子组件内使用【buyChild】 这个 【prop 属性】-->
 		
 
-		<!-- 🧾提交表单: v-on: -> @  绑定事件, 监听【子组件】的事件，⚡️⚡️如果发生了 add 事件就调用【父组件】的【TopAdd】事件-->
-		<app-form @add="topAdd"  v-bind:foodData="filtersFn.foodList"> </app-form>
+		<!-- 🧾提交表单: v-on: -> @  绑定事件来监听【app-form 子组件】的事件，⚡️⚡️如果【子组件使用了 add 方法】就使用所绑定的【父组件的 topAdd 方法】
+				v-on:add 相当于让子组件的 add = 父组件的 topAdd
+		-->
+		<app-form v-on:add="topAdd"  v-bind:foodData="filtersFn.foodList"> </app-form>
 
 		<!-- 👛 未购零食: 🔥 在子组件内定义  headline 的 prop 属性, 然后就可以在这个【父组件内】传值（使用父组件的数据）了, 然后⚡️分别访问两组数据:	filtersFn -> afterBuy -->
 		<app-section-list headline="👛 Did not purchased"  v-bind:buyChild="filtersFn.beforeBuy"> </app-section-list>
@@ -59,14 +63,14 @@ export default {
 				foodList: this.foods
 			}
 		}
-		/*  beforeBuy() { //未购买的表达式
+		/*  
+			beforeBuy() { //未购买的表达式
 		 		return this.foods.filter(item => !item.purchased)
 		 	},
 			afterBuy() { //未购买的表达式
 		 		return this.foods.filter(item => item.purchased)
-			}*/
+		}*/
 	},
-
 
 
 
